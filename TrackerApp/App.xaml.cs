0ae8cc25fd98cee50394b7cfd104e3f0936d8007
Serena W <50899598+sewoller@users.noreply.github.com>
2024-02-17 20:17:15 -0800

    first commit

---
+++ TrackerApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TrackerApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TrackerApp/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TrackerApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
